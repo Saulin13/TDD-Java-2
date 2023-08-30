94011a923e15e4fd6a4b94112b6c3734e86a0c19
Saulo Pereira Vasconcelos <saulovasconcelos07@gmail.com>
2023-08-30 17:15:51 -0300

    Pastas e Classes Criadas

---
+++ Test/com/exemplo/CalculadoraTest.java
@@ -0,0 +1,16 @@
+package com.exemplo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculadoraTest {
+
+	Calculadora calc = new Calculadora();
+	
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
